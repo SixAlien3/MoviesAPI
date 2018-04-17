c85ed1d646867b95b846f387343f34fa6cb0aa61
Abhilash Reddy <abhilash.reddy@outlook.com>
2018-04-17 17:07:39 -0400

    New Properties

--- Movies.Models/Movie.cs
+++ Movies.Models/Movie.cs
@@ -9,7 +9,8 @@ namespace Movies.Models
 {
     public class Movie : AuditableEntity
     {
-        [JsonIgnore] public string OriginalTitle { get; set; }
+        [JsonIgnore]
+        public string OriginalTitle { get; set; }
 
         [Index(IsUnique = false)]
         [StringLength(128)]
@@ -19,10 +20,14 @@ namespace Movies.Models
         public string Tagline { get; set; }
         public decimal Budget { get; set; }
         public decimal Revenue { get; set; }
-        [JsonProperty("homepage")] public string HomePage { get; set; }
+
+        [JsonProperty("homepage")]
+        public string HomePage { get; set; }
+
         public string ImdbId { get; set; }
 
-        [JsonIgnore] public string TmdbId { get; set; }
+        [JsonIgnore]
+        public string TmdbId { get; set; }
 
         public string PosterUrl { get; set; }
         public string BackdropUrl { get; set; }
@@ -33,20 +38,24 @@ namespace Movies.Models
         public DateTime? ReleaseDate { get; set; }
         public int RunTime { get; set; }
 
-        [JsonIgnore] public bool? IsReleased { get; set; }
+        [JsonIgnore]
+        public bool? IsReleased { get; set; }
 
         public decimal? AverageVote { get; set; }
 
-        [JsonIgnore] public int? VoteCount { get; set; }
+        [JsonIgnore]
+        public int? VoteCount { get; set; }
 
 
         public int ExternalId { get; set; }
 
         public ICollection<Genre> Genres { get; set; }
 
-        [JsonIgnore] public ICollection<MovieCasts> MovieCastses { get; set; }
+        [JsonIgnore]
+        public ICollection<MovieCasts> MovieCastses { get; set; }
 
-        [JsonIgnore] public ICollection<MovieCrew> MovieCrews { get; set; }
+        [JsonIgnore]
+        public ICollection<MovieCrew> MovieCrews { get; set; }
 
         public ICollection<Keyword> Keywords { get; set; }
 
